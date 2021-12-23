269e204489983602ae532a0742dc33ad09e848c5
omarcostahamido <omarcostinha@gmail.com>
2021-12-23 13:15:33 +0000

    Update pvm.py
    -bring back vlc
    -added 4 commands to control vlc: start, stop, fullscreen, set_position

--- pvm.py
+++ pvm.py
@@ -1,8 +1,8 @@
-# import vlc
+import vlc
 # from pythonosc import dispatcher, osc_server, udp_client
 import socket
 
-# media = vlc.MediaPlayer("jellyfish720p.mp4", "--intf macosx")
+media = vlc.MediaPlayer("jellyfish720p.mp4")
 
 #media.set_fullscreen(True)
 
@@ -33,10 +33,21 @@ sock.bind((UDP_IP, UDP_PORT))
 
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    if data=="john":
-    	print("welcome john")
+    data = data.decode('UTF-8').split()
+    command = data[0]
+    argument = data[1]
+    if command=="start":
+    	media.play()
+    	pass
+    if command=="stop":
+    	media.stop()
+    	pass
+    if command=="set_position":
+		media.set_position(float(data[1]))
+    	pass
+    if command=="fullscreen":
+    	# media.set_fullscreen(True)
+    	media.toggle_fullscreen()
     	pass
     else:
-    	print("something else arrived")
-
-    # print("received message: %s" % data)
+    	print("I received command \"%s\" but I don't know what to do with it, yet." % data)
