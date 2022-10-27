2558836175c9b43469184477624bc678db86da04
Kane <ckq257@gmail.com>
2022-10-27 12:49:02 -0700

    Revise pdsh script

--- deploy_code_to_rpi.sh
+++ deploy_code_to_rpi.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Get all host ip and save it in to a file
-cat max-init.txt | awk '{split($0,a," "); print a[3]}' &>> host_ip.txt
+cat max-init.txt | awk '{split($0,a," "); print a[3]}' >> host_ip.txt 2>&1
 
 # Note: run `brew install pdsh` before you run the command below
 # ssh into multiple host
-pdsh -R ssh -w host_ip.txt
-
-cd /home/pi
+# setup passwordless rpi first
+# please follow: https://danidudas.medium.com/how-to-connect-to-raspberry-pi-via-ssh-without-password-using-ssh-keys-3abd782688a
+pdsh -R ssh -w ^host_ip.txt -l pi
 
 # kill all python process
 pkill -9 -f pvm_omx.py
