129eb41c9b93ae2b32995ad6e0e5a9fbd0f88eda
Kane <ckq257@gmail.com>
2022-11-28 11:32:54 -0800

    Fix indentation

--- pvm.py
+++ pvm.py
@@ -196,9 +196,10 @@ if __name__ == '__main__':
 	global PORT
 	global _logger
 	p = argparse.ArgumentParser()
-	p.add_argument('--port', type=int, nargs='?', default=8001, help='The port that pvm.py will use to receive control messages. '
-																		'Using the default port 8001 to control the first display. '
-																		'Using ny other port to control the second display.')
+	p.add_argument('--port', type=int, nargs='?', default=8001, help=
+					'The port that pvm.py will use to receive control messages. '
+					'Use the default port 8001 to control the first display. '
+					'Use any other port to control the second display.')
 	args = p.parse_args()
 	PORT = args.port
 	_init_logger()
